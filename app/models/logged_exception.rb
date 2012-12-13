85fa67d7024edf2d48387619b71844f187cf374f
Nicolas Despres <nicolas.despres@gmail.com>
2012-12-13 15:50:30 +0100

    Make attributes accessible.

--- app/models/logged_exception.rb
+++ app/models/logged_exception.rb
@@ -11,11 +11,14 @@ class LoggedException < ActiveRecord::Base
         :backtrace       => exception.backtrace,
         :request         => controller.request
     end
-    
+
     def host_name
       `hostname -s`.chomp
     end
   end
+
+  attr_accessible :exception_class, :controller_name, :action_name,
+                  :message, :backtrace, :request
 
   scope :by_exception_class, lambda {|exception_class| where(:exception_class => exception_class)}
   scope :by_controller_and_action, lambda {|controller_name, action_name| where(:controller_name => controller_name, :action_name => action_name)}
@@ -24,11 +27,11 @@ class LoggedException < ActiveRecord::Base
   scope :message_like, lambda {|query|  where(:message.matches => "%#{query}%")}
   scope :days_old, lambda {|day_number| where("created_at >= ?", day_number.to_f.days.ago.utc)}
   scope :sorted, order("created_at desc")
-  
+
   def name
     "#{self.exception_class} in #{self.controller_action}"
   end
-  
+
   def backtrace=(trace)
     trace = sanitize_backtrace(trace) * "\n" unless trace.is_a?(String)
     write_attribute :backtrace, trace
@@ -43,7 +46,7 @@ class LoggedException < ActiveRecord::Base
         env << '* ' + ("%-*s: %s" % [max.length, key, request.env[key].to_s.strip])
       end
       write_attribute(:environment, (env << "* Process: #{$$}" << "* Server : #{self.class.host_name}") * "\n")
-      
+
       write_attribute(:request, [
           "* URL:#{" #{request.method.to_s.upcase}" unless request.get?} #{request.protocol}#{request.env["HTTP_HOST"]}#{request.fullpath}",
           "* Format: #{request.format.to_s}",
@@ -60,11 +63,11 @@ class LoggedException < ActiveRecord::Base
   def self.class_names
     select("DISTINCT exception_class").order(:exception_class).collect(&:exception_class)
   end
-  
+
   def self.controller_actions
     select("DISTINCT controller_name, action_name").order(:controller_name,:action_name).collect(&:controller_action)
   end
-  
+
   private
   @@rails_root      = Pathname.new(Rails.root).cleanpath.to_s
   @@backtrace_regex = /^#{Regexp.escape(@@rails_root)}/
